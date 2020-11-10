6c1f48c532c1bfe01f77c77fdae6fee035dff7b4
TECHN0ID <nichham2@hotmail.com>
2020-11-09 23:16:56 -0500

    added form data

--- includes/header.php
+++ includes/header.php
@@ -7,12 +7,14 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="styleshet" href="css/site.css" />
+    <link rel="stylesheet" href="css/site.css" />
+
     <title>Attendace - <?php echo $title ?></title>
+
   </head>
-  
+  <!----- navigation bar ----->
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#"><?php echo $title ?></a>
+        <a class="navbar-brand" href="index.php"><?php echo $title ?></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -25,5 +27,6 @@
             </div>
         </div>
     </nav>
+ <!----- start of body content----->
   <body>
-   
+    <div class="container">
--- index.php
+++ index.php
@@ -1,10 +1,80 @@
 <?php 
-$title = 'Home';
+$title = 'Registration Form';
 require_once'includes/header.php';
-
-
 ?>
+<!------
+first name 
+last name
+date of birth (use date picker)
+specialty (data base admin soft developer web admin )
+email address 
+contact number 
 
-<?php require_once'includes/footer.php';
+----->
+<h1 class="text-center" >Registration for IT Confrence</h1>
 
-?>
+<form class="container">
+    
+    <div class="form-row">
+        <div class="col">
+            <label for="firstname">First Name</label>
+            <input type="text" class="form-control" id="firstname">
+        </div>
+
+        <div class="col">
+            <label for="lastname">Last Name</label>
+            <input type="text" class="form-control" id="lastname">
+        </div>
+    </div>
+    
+    <br/>
+    <div class="form-row">
+        <div class="col">
+            <label for="dateofbith">Date of Birth</label>
+            <input type="date" class="form-control" id="dateofbith">
+            <small id="dateofbirthhelp" class="form-text text-muted"></small>
+        </div>
+
+        <div class="col">
+            <label for="exampleFormControlSelect1"></label>
+            <select class="form-control" id="exampleFormControlSelect1">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            </select>
+        </div>
+    </div>
+
+    <br/>
+    
+    <div class="form-group">
+        <label for="emailaddress">Email address</label>
+        <input type="email" class="form-control" id="emailaddress" aria-describedby="emailHelp">
+        <small id="emailHelp" class="form-text text-muted">Don't worry this is between me and you ;-).</small>
+    </div>
+
+    <div class="form-group">
+        <label for="contactnumber">Phone number</label>
+        <input type="number" class="form-control" id="contactnumber">
+        <small id="phonenumberHelp" class="form-text text-muted">We won't call you so don't ask.</small>
+    </div>
+
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password">
+        <small id="passwordHelp" class="form-text text-muted">Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.</small>
+    </div>
+    
+    <!----<div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div> ---->
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+  
+</form>
+
+
+<?php require_once'includes/footer.php'; ?>
